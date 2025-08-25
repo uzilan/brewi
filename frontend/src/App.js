20583f9ac80767a68768cc8e3b5821900f639236
Uzi Landsmann <uzilan@Uzis-MacBook-Pro.local>
2025-08-25 09:07:32 +0200

    Update application title to 'Brewi'
    - Changed HTML title from 'Brewanator' to 'Brewi'
    - Updated meta description to use 'Brewi' branding
    - Changed app header title in toolbar to 'Brewi'
    - Consistent branding throughout the application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -508,7 +508,7 @@ function App() {
       <AppBar position='static'>
         <Toolbar>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Brewanator
+            Brewi
           </Typography>
           <Button
             color='inherit'
